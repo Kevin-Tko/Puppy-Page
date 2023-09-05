3b572290a834f7c2cba33bf436ba2e6623d654d4
Kevin-Tko <kelvnjogu@gmail.com>
2023-09-05 20:13:31 +0300

    commit

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, redirect, url_for
+import os
+from sqlalchemy import SQLAlchemy
+from flask_migrate  import Migrate
 
 
 
-######Creating my app######
+######  Creating my app  ######
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'mysecretkey'
 
-def name():
-    print('Kevin')
-name()
+######   Creating my SQLITE database and configuring my app ######
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
