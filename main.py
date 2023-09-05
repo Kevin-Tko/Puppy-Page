8146deec9d49160033ddeb25e1abec0703e660bf
Kevin-Tko <kelvnjogu@gmail.com>
2023-09-05 19:36:06 +0300

    commit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, redirect, url_for
 
-def name():
-    print('Kevin')
+
+
 ######Creating my app######
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'mysecretkey'
 
-
+def name():
+    print('Kevin')
+name()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
